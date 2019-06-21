20f9c60a811b86a257603aeaf061ce098204b69e
zzswang <zzswang@gmail.com>
2019-06-21 13:52:28 +0800

    fix: add gps error vehicle state

--- mock/constants.js
+++ mock/constants.js
@@ -1,4 +1,4 @@
-const State = ["UNKNOWN", "ONLINE", "OFFLINE"]; // "未知（从未登录）, 在线， 离线"
+const State = ["UNKNOWN", "ONLINE", "OFFLINE", "GPS_ERROR"]; // "未知（从未登录）, 在线， 离线"
 
 module.exports = {
   State,
